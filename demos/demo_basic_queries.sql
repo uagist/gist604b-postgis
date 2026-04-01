bb7b66bb9afc841e76e105e6c5ce98c5bce4c8ec
Yoga Korgaonkar <74412590+yoganandk@users.noreply.github.com>
2026-04-01 10:39:34 -0700

    Update and rename demo_basic_sql.sql to demo_basic_queries.sql
    Updated query examples

---
+++ demos/demo_basic_queries.sql
@@ -0,0 +1,34 @@
+-- SELECT statement - select data from table
+
+-- All columns from the nyc_neighborhoods table
+SELECT * FROM nyc_neighborhoods;
+
+-- Column name from the nyc_neighborhoods table
+SELECT name FROM nyc_neighborhoods;
+
+-- Columns name, boroname from the nyc_neighborhoods table
+SELECT name, boroname FROM nyc_neighborhoods;
+
+-- SELECT DISTINCT - select unique values
+SELECT DISTINCT name
+FROM nyc_neighborhoods;
+
+-- ORDER BY - sort ascending/descending
+-- Order by the name - default is ascending
+SELECT 
+  name 
+FROM nyc_neighborhoods
+ORDER BY name;
+
+-- Order by the name - ascending
+SELECT 
+  name 
+FROM nyc_neighborhoods
+ORDER BY name ASC;
+
+-- Order by the name - descending
+SELECT 
+  name 
+FROM nyc_neighborhoods
+ORDER BY name DESC;
+
